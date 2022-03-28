edecaaa877a6be261699b89080387316d8b04fc7
victorverma <vishashv@gmail.com>
2022-03-28 17:02:07 -0400

    Add func to make main tbl w all info

--- dgd.R
+++ dgd.R
@@ -6,3 +6,20 @@ calc_grad <- function(f) {
   force(f)
   function(x) grad(f, x)
 }
+
+make_main_tbl <- function(f_list,
+                          grad_list = NULL,
+                          init_xs,
+                          init_step_size,
+                          weight_mat) {
+  if (is.null(grad_list)) {
+    grad_list <- map(f_list, calc_grad)
+  }
+  tibble(
+    f = f_list,
+    grad = grad_list,
+    curr_x = init_xs,
+    step_size = init_step_size,
+    weights = apply(weight_mat, 1, identity, simplify = FALSE)
+  )
+}
