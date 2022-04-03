d569615e2252c0a7404ac9925951662a43d5662e
victorverma <vishashv@gmail.com>
2022-04-02 20:13:12 -0400

    Add func to calc new x

--- dgd.R
+++ dgd.R
@@ -27,3 +27,24 @@ make_main_tbl <- function(f_list,
     curr_step_size = init_step_size
   )
 }
+
+calc_next_x <- function(id_tbl, context) {
+  for (nm in names(context)) {
+    assign(nm, context[[nm]], envir = .GlobalEnv) 
+  }
+  
+  id <- id_tbl$id
+  weights <- main_tbl$weights[[id]]
+  curr_xs <- main_tbl$curr_x
+  curr_x <- curr_xs[[id]]
+  curr_step_size <- main_tbl$curr_step_size[id]
+  grad_ <- main_tbl$grad[[id]]
+  curr_weighted_sum <- Reduce(
+    `+`, mapply(`*`, weights, curr_xs, SIMPLIFY = FALSE)
+  )
+  next_x <- curr_weighted_sum - curr_step_size * grad_(curr_x)
+  
+  l <- as.list(next_x)
+  names(l) <- paste0("x", seq_along(next_x))
+  as.data.frame(l)
+}
